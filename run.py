503115ffaff30461265b34a31925d2dab548f4df
i_Zolox <33046239+izolox@users.noreply.github.com>
2024-10-18 21:15:26 -0500

    create database if not exist

--- run.py
+++ run.py
@@ -9,12 +9,26 @@ from widget import *
 
 async def main():
     
+    #create datavase if it doesn't exist
     connection = pymysql.connect(
         host = config.DB_HOST,
         user = config.DB_USER,
-        password = config.DB_PASSWORD,
-        database = config.DB_NAME
+        password = config.DB_PASSWORD
     )
+    
+    with connection.cursor() as cursor:
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {config.DB_NAME};")
+        cursor.execute(f"USE {config.DB_NAME};")
+        cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS creatures (
+                id VARCHAR(50) PRIMARY KEY,
+                name VARCHAR(50) DEFAULT "Unknown" NOT NULL,
+                value_min INT DEFAULT 0 NOT NULL,
+                value_max INT DEFAULT 0 NOT NULL,
+                demand INT DEFAULT 0 NOT NULL,
+                stability ENUM('UNKNOWN', 'LOWERING', 'RISING', 'STABLE', 'UNSTABLE') DEFAULT 'UNKNOWN' NOT NULL
+            );"""
+        )
     
     input("Would you like to update the value data? (y/n): ")
     
