09674461ad689ec098f4d76e11b6dcaecc328270
i_Zolox <33046239+izolox@users.noreply.github.com>
2024-10-18 21:07:48 -0500

    create install.bat

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Installing dependencies...
+
+:: Install Python dependencies
+pip install -r requirements.txt
+
+:: Any other installation steps can be added here
+
+echo Installation complete.
+pause
